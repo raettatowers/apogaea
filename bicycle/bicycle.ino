811498f95c56d6b6831159a0f97b89eb0da80746
Brandon Skari <brandon@skari.org>
2021-07-15 19:22:34 -0600

    Start adding spectrum analyzer

--- bicycle/bicycle.ino
+++ bicycle/bicycle.ino
@@ -1,32 +1,13 @@
 #include <FastLED.h>
+#include <Adafruit_CircuitPlayground.h>
+
 #include "animations.hpp"
-
-// How many leds in your strip?
-#define NUM_LEDS 57
-
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-const int DATA_PIN = 7;
-const int CLOCK_PIN = 6;
-const int BUTTON_PIN = 11;
-
+#include "constants.hpp"
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
-InchWormAnimation worm1(leds, NUM_LEDS, 9, 0);
-InchWormAnimation worm2(leds, NUM_LEDS, 9, NUM_LEDS / 2);
-UsaColors usaColors;
-RainbowColors rainbowColors(1, 1);
-RainbowColors iteratingColors1(20, 20), iteratingColors2(20, 20);
-
-const int NUM_COLOR_FUNCTORS = 3;
-ColorFunctor* colorFunctors[][2] = {
-  {&usaColors, &usaColors},
-  {&rainbowColors, &rainbowColors},
-  {&iteratingColors1, &iteratingColors2}
-};
-
+RainbowColors iteratingColors(20, 20);
+SpectrumAnalyzer analyzer(leds, NUM_LEDS);
 
 void setup() {
   FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR>(leds, NUM_LEDS);
@@ -34,9 +15,11 @@ void setup() {
   fill_solid(leds, NUM_LEDS, CRGB::Black);
   pinMode(CLOCK_PIN, OUTPUT);
   pinMode(DATA_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(CPLAY_REDLED, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
   Serial.begin(9600);
+  CircuitPlayground.begin();
 }
 
 
@@ -49,26 +32,10 @@ uint8_t colorFunctorIndex = 0;
 
 void loop() {
   // Do animations here
-  ColorFunctor* functor1 = colorFunctors[colorFunctorIndex][0];
-  ColorFunctor* functor2 = colorFunctors[colorFunctorIndex][1];
-  worm1.animate(*functor1);
-  functor1->reset();
-  worm2.animate(*functor2);
-  functor2->reset();
 
   FastLED.show();
-  // Vary the speed of the animation
-  sleepTime_ms += sleepTimeStep_ms;
-  if (sleepTime_ms >= maxSleepTime_ms) {
-    sleepTimeStep_ms = -sleepTimeIncrement_ms;
-  } else if (sleepTime_ms <= minSleepTime_ms) {
-    sleepTimeStep_ms = sleepTimeIncrement_ms;
-  }
-  auto start = millis();
-  while (millis() < start + sleepTime_ms) {
-    checkAndHandleButton();
-  }
   fill_solid(leds, NUM_LEDS, CRGB::Black);
+  analyzer.animate(iteratingColors);
 }
 
 
@@ -77,23 +44,4 @@ void checkAndHandleButton() {
   static bool pressed = false;
 
   int buttonState = digitalRead(BUTTON_PIN);
-  if (buttonState == HIGH) {
-    // Not pressed
-    pressed = false;
-    digitalWrite(LED_BUILTIN, LOW);
-    return;
-  }
-
-  if (debounceTime_ms - millis() < 25) {
-    // Just a bounce, ignore it
-    return;
-  }
-
-  debounceTime_ms = millis();
-  // Avoid continual presses
-  if (!pressed) {
-    pressed = true;
-    digitalWrite(LED_BUILTIN, HIGH);
-    colorFunctorIndex = (colorFunctorIndex + 1) % NUM_COLOR_FUNCTORS;
-  }
 }
