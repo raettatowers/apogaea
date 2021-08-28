dc3187ccbc99a912fd39659b34556f677b8da337
Brandon Skari <brandon@skari.org>
2021-08-28 15:58:40 -0600

    Typecast COUNT_OF to int to ignore unsigned comparison warnings

--- vest/constants.hpp
+++ vest/constants.hpp
@@ -1,7 +1,8 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
-#define COUNT_OF(x) (sizeof((x)) / sizeof(0[(x)]))
+// Cast to int so that I don't get unsigned comparison warnings
+#define COUNT_OF(x) static_cast<int>(sizeof((x)) / sizeof(0[(x)]))
 
 // Generated from offsets.py
 #include "offsets.hpp"
