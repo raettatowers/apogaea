94fb5bebdf38eb71cbf037660275b219c9ae3a47
Brandon Skari <brandon@skari.org>
2021-08-28 22:31:35 -0600

    Switch to using signed ints for offsets

--- vest/offsets.py
+++ vest/offsets.py
@@ -23,12 +23,12 @@ for x in range(len(led_counts) - 1, -1, -1):
     for y in range(led_counts[x], max(led_counts)):
         array[-1].append("UNUSED_LED")
 
-print("const int UNUSED_LED = 255;")
+print("const int UNUSED_LED = -1;")
 print(f"const int LED_COUNT = {sum(led_counts) * 2};")
 print(f"const int LED_COLUMN_COUNT = 2 * {len(led_counts)};")
 print(f"const int LED_ROW_COUNT = {max(led_counts)};")
 print("// x first then y, starting at lower left corner")
-print(f"const uint16_t_t LED_STRIPS[2 * {len(led_counts)}][{max(led_counts)}] = {{")
+print(f"const int16_t LED_STRIPS[2 * {len(led_counts)}][{max(led_counts)}] = {{")
 for x in range(len(array)):
     items = [array[x][y] for y in range(len(array[x]))]
     print(f"    {{{', '.join(items)}}},")
