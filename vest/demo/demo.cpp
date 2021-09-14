58d7b0a4a20a4400d7aad9738e1a6f120af12d13
Brandon Skari <brandon@skari.org>
2021-09-14 02:24:22 -0600

    Move Bidoulle ring offsets calculation to a Python script

--- vest/demo/demo.cpp
+++ vest/demo/demo.cpp
@@ -151,22 +151,6 @@ void p3only(int time, setLed_t setLed, SDL_Renderer *const renderer) {
 
 void p4only(int time, setLed_t setLed, SDL_Renderer *const renderer) {
   // Oops, I think this is doing v3 of Bidoulle
-  static bool precomputed = false;
-  static uint8_t rings[LED_COLUMN_COUNT * 2][LED_ROW_COUNT * 2];
-  if (!precomputed) {
-    for (int x = -LED_COLUMN_COUNT / 2; x < 3 * LED_COLUMN_COUNT / 2; ++x) {
-      for (int y = -LED_ROW_COUNT / 2; y < 3 * LED_ROW_COUNT / 2; ++y) {
-        int xSqr = x - LED_COLUMN_COUNT / 2;
-        xSqr *= xSqr;
-        int ySqr = y - LED_ROW_COUNT / 2;
-        ySqr *= ySqr;
-        const uint8_t value = (sinf(sqrtf(.1f * (xSqr + ySqr) + 1)) + 1) * 127;
-        rings[x + LED_COLUMN_COUNT / 2][y + LED_ROW_COUNT / 2] = value;
-      }
-    }
-    precomputed = true;
-  }
-
   const uint8_t offset = time / 1000;
   const int xOffset = sin16(time / 2) / 4096;
   const int yOffset = sin16(time / 3) / 8192;
@@ -174,7 +158,7 @@ void p4only(int time, setLed_t setLed, SDL_Renderer *const renderer) {
     for (int y = 0; y < LED_ROW_COUNT; ++y) {
       const int adjustedX = x + xOffset + LED_COLUMN_COUNT / 2;
       const int adjustedY = y + yOffset + LED_ROW_COUNT / 2;
-      setLed(x, y, rings[adjustedX][adjustedY] + offset, renderer);
+      setLed(x, y, bidoulleV3rings[adjustedX][adjustedY] + offset, renderer);
     }
   }
 }
@@ -426,23 +410,6 @@ const char *plasmaBidoulle(int, SDL_Renderer *const renderer) {
 
 const char *plasmaBidoulleFast(int time, SDL_Renderer *const renderer) {
   const uint16_t multiplier = 0.15f * PI_16_1_0;
-  static bool precomputed = false;
-  static uint16_t rings[LED_COLUMN_COUNT * 2][LED_ROW_COUNT * 2];
-
-  if (!precomputed) {
-    for (int x = -LED_COLUMN_COUNT / 2; x < 3 * LED_COLUMN_COUNT / 2; ++x) {
-      for (int y = -LED_ROW_COUNT / 2; y < 3 * LED_ROW_COUNT / 2; ++y) {
-        int xSqr = x - LED_COLUMN_COUNT / 2;
-        xSqr *= xSqr;
-        int ySqr = y - LED_ROW_COUNT / 2;
-        ySqr *= ySqr;
-        const uint16_t value = (sinf(sqrtf(.1f * (xSqr + ySqr) + 1)) + 1) * 32767;
-        rings[x + LED_COLUMN_COUNT / 2][y + LED_ROW_COUNT / 2] = value;
-      }
-    }
-    precomputed = true;
-  }
-
   const int xOffset = sin16(time / 2) / 4096;
   const int yOffset = sin16(time / 3) / 8192;
   for (int x = 0; x < LED_COLUMN_COUNT; ++x) {
@@ -454,7 +421,7 @@ const char *plasmaBidoulleFast(int time, SDL_Renderer *const renderer) {
             16384); // bad values, but looks good?
         const int adjustedX = x + xOffset + LED_COLUMN_COUNT / 2;
         const int adjustedY = y + yOffset + LED_ROW_COUNT / 2;
-        const int16_t v3 = rings[adjustedX][adjustedY];
+        const int16_t v3 = bidoulleV3rings[adjustedX][adjustedY];
         const int16_t v = v1 + v2 + v3;
         const uint8_t red = (sin16(v) / 256) + 128;
         const int16_t green = (sin16(v + 2 * 32768 / 3) / 256) + 128;
--- vest/offsets.py
+++ vest/offsets.py
@@ -1,35 +1,63 @@
-# Generates C code for the array offsets
-# The generated offsets
+"""Generates C code for various things."""
+import math
+import sys
+
 led_counts = [6, 7, 10, 11, 12, 12, 6, 6, 7, 11, 11, 11, 11, 11]
-array = []
-count = 0
-for x in range(len(led_counts)):
-    array.append([])
-    for y in range(led_counts[x]):
-        array[-1].append(str(count))
-        count += 1
-    if len(array) % 2 == 0:
-        array[-1] = list(reversed(array[-1]))
-    for y in range(led_counts[x], max(led_counts)):
-        array[-1].append("UNUSED_LED")
 
-for x in range(len(led_counts) - 1, -1, -1):
-    array.append([])
-    for y in range(led_counts[x]):
-        array[-1].append(str(count))
-        count += 1
-    if len(array) % 2 == 0:
-        array[-1] = list(reversed(array[-1]))
-    for y in range(led_counts[x], max(led_counts)):
-        array[-1].append("UNUSED_LED")
+def print_offsets() -> None:
+    """Generates C code for the array offsets."""
+    array = []
+    count = 0
+    for x in range(len(led_counts)):
+        array.append([])
+        for y in range(led_counts[x]):
+            array[-1].append(str(count))
+            count += 1
+        if len(array) % 2 == 0:
+            array[-1] = list(reversed(array[-1]))
+        for y in range(led_counts[x], max(led_counts)):
+            array[-1].append("UNUSED_LED")
 
-print("const int UNUSED_LED = -1;")
-print(f"const int LED_COUNT = {sum(led_counts) * 2};")
-print(f"const int LED_COLUMN_COUNT = 2 * {len(led_counts)};")
-print(f"const int LED_ROW_COUNT = {max(led_counts)};")
-print("// x first then y, starting at lower left corner")
-print(f"const int16_t LED_STRIPS[2 * {len(led_counts)}][{max(led_counts)}] = {{")
-for x in range(len(array)):
-    items = [array[x][y] for y in range(len(array[x]))]
-    print(f"    {{{', '.join(items)}}},")
-print("};")
+    for x in range(len(led_counts) - 1, -1, -1):
+        array.append([])
+        for y in range(led_counts[x]):
+            array[-1].append(str(count))
+            count += 1
+        if len(array) % 2 == 0:
+            array[-1] = list(reversed(array[-1]))
+        for y in range(led_counts[x], max(led_counts)):
+            array[-1].append("UNUSED_LED")
+
+    print("const int UNUSED_LED = -1;")
+    print(f"const int LED_COUNT = {sum(led_counts) * 2};")
+    print(f"const int LED_COLUMN_COUNT = 2 * {len(led_counts)};")
+    print(f"const int LED_ROW_COUNT = {max(led_counts)};")
+    print("// x first then y, starting at lower left corner")
+    print(f"const int16_t LED_STRIPS[2 * {len(led_counts)}][{max(led_counts)}] = {{")
+    for x in range(len(array)):
+        items = [array[x][y] for y in range(len(array[x]))]
+        print(f"    {{{', '.join(items)}}},")
+    print("};")
+
+
+def print_precomputed_bidoulle_v3() -> None:
+    """Prints the precomputed Bidoulle v3 values."""
+    LED_COLUMN_COUNT = 2 * len(led_counts)
+    LED_ROW_COUNT = max(led_counts)
+
+    print("const uint8_t bidoulleV3rings[LED_COLUMN_COUNT * 2][LED_ROW_COUNT * 2] = {")
+
+    for x in range(-LED_COLUMN_COUNT // 2, 3 * LED_COLUMN_COUNT // 2):
+        sys.stdout.write("  {")
+        for y in range(-LED_ROW_COUNT // 2, 3 * LED_ROW_COUNT // 2):
+            xSqr = (x - LED_COLUMN_COUNT // 2) ** 2
+            ySqr = (y - LED_ROW_COUNT // 2) ** 2
+            value = round((math.sin(math.sqrt(0.1 * (xSqr + ySqr) + 1.0)) + 1.0) * 127)
+            sys.stdout.write(f"{value}, ")
+        print("},")
+    print("};");
+
+
+if __name__ == "__main__":
+    print_offsets()
+    print_precomputed_bidoulle_v3()
