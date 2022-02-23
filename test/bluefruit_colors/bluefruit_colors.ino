c3aed2484b327568410eabc4d2418c9ff810da0f
Brandon Skari <brandon@skari.org>
2022-02-22 20:34:46 -0700

    Make Bluefruit test work with external LEDs

--- test/bluefruit_colors/bluefruit_colors.ino
+++ test/bluefruit_colors/bluefruit_colors.ino
@@ -3,20 +3,26 @@
 
 #include "capacitive_touch.hpp"
 
-const int LED_COUNT = 10;
+const int INTERNAL_LED_COUNT = 1;
+const int EXTERNAL_LED_COUNT = 5;
 
 const int NEOPIXEL_PIN = 8;
+const int EXTERNAL_PIN = 16; // 16 = A2 / D9
+const int DATA_PIN = 16; // 16 = A2 / D9
+const int CLOCK_PIN = 15; // 15 = A1 / D6
 const int LEFT_BUTTON_PIN = 4;
 const int RIGHT_BUTTON_PIN = 5;
 const int LEFT_PAD_PIN = 0;
 const int RIGHT_PAD_PIN = 6;
 const int SWITCH_PIN = 7;
 
-CRGB leds[LED_COUNT];
+CRGB internalLeds[INTERNAL_LED_COUNT];
+CRGB externalLeds[EXTERNAL_LED_COUNT];
 
 void setup() {
   Serial.begin(115200);
-  LEDS.addLeds<WS2812B, NEOPIXEL_PIN, GRB>(leds, LED_COUNT);
+  LEDS.addLeds<WS2812B, NEOPIXEL_PIN, GRB>(internalLeds, INTERNAL_LED_COUNT);
+  LEDS.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(externalLeds, EXTERNAL_LED_COUNT);
   LEDS.setBrightness(255);
   pinMode(LEFT_BUTTON_PIN, INPUT_PULLDOWN);
   pinMode(RIGHT_BUTTON_PIN, INPUT_PULLDOWN);
@@ -29,7 +35,6 @@ void loop() {
   static uint8_t red;
   static uint8_t green;
   static uint8_t blue;
-  static int index = 0;
   static TouchIn leftPad(LEFT_PAD_PIN);
   static TouchIn rightPad(RIGHT_PAD_PIN);
 
@@ -58,7 +63,8 @@ void loop() {
   }
 
   FastLED.clear();
-  fill_solid(leds, LED_COUNT, CRGB(red, green ,blue));
+  fill_solid(internalLeds, INTERNAL_LED_COUNT, CRGB(red, green ,blue));
+  fill_solid(externalLeds, EXTERNAL_LED_COUNT, CRGB(red, green ,blue));
   FastLED.show();
 
   if (needDelay) {
